beba613349558be38846392ef5d4a61d8464a8c6
Pep Creus Costa <pep.creuscosta@gmail.com>
2019-07-11 20:51:06 +0200

    Added a script that calculates the average of another document.
    The first file ('data.txt') contains a list of two columns with numbers. The second file ('mitjanes.py') calculates the average of each column and prints their values.

---
+++ mitjanes.py
@@ -0,0 +1,22 @@
+column1 = []
+column2 = []
+
+def mitjana(list_of_numbs):
+  total_sum = 0
+  #One way of doing the average
+  #for i in list_of_numbs:
+  #total_sum = total_sum + int(i)
+  
+  #A second way of doing the average (by using sum(int(i) for i in list_of_numbs)) 
+  total_sum = sum(int(i) for i in list_of_numbs)
+  print(float(total_sum)/len(list_of_numbs))
+
+with open('data.txt', 'r') as f:
+  for line in f:
+    if line[-1] == "\n":
+      clean_line = line.rstrip()
+    column1.append(clean_line.split()[0])
+    column2.append(clean_line.split()[1])
+
+mitjana(column1)
+mitjana(column2)
