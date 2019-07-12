235b36a7425b8d2865fce1bb95bbf1f97cf756d4
Pep Creus Costa <pep.creuscosta@gmail.com>
2019-07-12 19:46:35 +0200

    Modified the file "agenda.py".
    Modified the function "afegir_contacte".

--- level1/agenda.py
+++ level1/agenda.py
@@ -10,12 +10,19 @@ import sys
 
 def afegir_contacte(dades):
   # Mode 'a' per fer append al nou fitxer
-  if len(dades) != 4
-  with open('', 'a') as f:
-  for i in dades:
-   
- #  
-def llegir_contacte():
+  if len(dades) != 4:
+    print ("Error")
+    sys.exit(1)
+  with open('agenda.txt', 'a') as f:
+    for i in dades:
+      if len(dades) < 4:
+        f.write(i)
+        f.write('string\t') 
+      elif len(dades) == 4:
+        f.write(i)
+        f.write('string\n')
+def llegir_contacte(contacte):
+
 def llegir_tots():
 
 if sys.argv[1] == "afegir":
