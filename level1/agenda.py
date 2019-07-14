38907622e4312c8766bf182876bc6809ee1cb552
Pep Creus Costa <pep.creuscosta@gmail.com>
2019-07-14 20:14:07 +0200

    Modified functions from "agenda.py".
    The following functions were added or modified: afegir_contacte, llegir_contacte, llegir_tots.

--- level1/agenda.py
+++ level1/agenda.py
@@ -13,23 +13,35 @@ def afegir_contacte(dades):
   if len(dades) != 4:
     print ("Error")
     sys.exit(1)
+
   with open('agenda.txt', 'a') as f:
     for i in dades:
-      if len(dades) < 4:
-        f.write(i)
-        f.write('string\t') 
-      elif len(dades) == 4:
-        f.write(i)
-        f.write('string\n')
+      f.write(i)
+      f.write('\t')
+    f.write('\n')
+
 def llegir_contacte(contacte):
+  if len(contacte) != 2:
+    print ("Error")
+  with open('agenda.txt', 'r') as f:
+    for line in f:
+      paraules = line.split('\t')
+      if contacte[0] == paraules[0] and contacte[1] == paraules[1]:
+          print line.rstrip()
+          return line
+    print("Error: no es troba el contacte")
 
 def llegir_tots():
+  with open('agenda.txt', 'r') as f:
+    for line in f:
+      print line.rstrip()
+
 
 if sys.argv[1] == "afegir":
   afegir_contacte(sys.argv[2:])
 
 elif sys.argv[1] == "llegir":
-  llegir_contacte(sys.argv[])
+  llegir_contacte(sys.argv[2:4])
 
 elif sys.argv[1] == "tots":
   llegir_tots()
